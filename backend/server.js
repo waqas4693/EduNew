de3b4867dc0ea84424bdd47685be3b37039ed558
waqas4693 <waqas4693@gmail.com>
2025-01-26 14:51:39 +0500

    Server Error Check Message

--- backend/server.js
+++ backend/server.js
@@ -94,6 +94,7 @@ await mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(process.env.PORT)
+    console.log(`Server running on port ${process.env.PORT}`)
   })
   .catch(error => console.log(`${error} did not connect`))
 
