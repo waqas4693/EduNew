70480e46aa3d5bdfa7a08dfe3451866d16041511
waqas4693 <waqas4693@gmail.com>
2025-02-05 16:35:56 +0500

    Url Updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api/'
-// const API_URL = 'https://edusupplements.co.uk/api/'
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api/'
+const API_URL = 'https://edusupplements.co.uk/api/'
 
 export const postData = async (endpoint, data, config = {}) => {
   try {
