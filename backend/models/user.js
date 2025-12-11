b37c87345d998df728328783a3021e3ff43f0126
waqas4693 <waqas4693@gmail.com>
2025-12-12 02:03:33 +0300

    Security Check 5

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,9 +7,9 @@ const userSchema = new mongoose.Schema(
       type: String
     },
     email: {
-      
+      // type: String,
       // required: true,
-      unique: true
+      // unique: true
     },
     password: {
       type: String,
