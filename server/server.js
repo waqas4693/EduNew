2a60e3e5e18c148d6f6d2ad9abcf96c52d464fdf
waqas4693 <waqas4693@gmail.com>
2026-09-12 03:43:02 +0500

    Update server and client configurations to allow Vite preview to listen on all network interfaces. Modify start script in package.json to specify host and port, ensuring accessibility from external sources.

--- server/server.js
+++ server/server.js
@@ -113,7 +113,7 @@ mongoose.set('strictQuery', false)
 await mongoose
   .connect(process.env.MONGO_URL)
   .then(async () => {
-    app.listen(process.env.PORT)
+    app.listen(process.env.PORT, '0.0.0.0')
     console.log(`Server running on port ${process.env.PORT}`)
   })
   .catch(error => console.log(`${error} did not connect`))
